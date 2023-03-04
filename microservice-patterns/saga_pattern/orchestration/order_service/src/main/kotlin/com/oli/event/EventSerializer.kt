3aa43cba31d6e8aa96993cf33b3040d232de10c9
Oliver <oliver.simon.schmid@gmail.com>
2023-03-04 22:21:32 +0100

    added create order saga events

--- microservice-patterns/saga_pattern/orchestration/order_service/src/main/kotlin/com/oli/event/EventSerializer.kt
+++ microservice-patterns/saga_pattern/orchestration/order_service/src/main/kotlin/com/oli/event/EventSerializer.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
+import kotlin.jvm.Throws
 
 
 object EventSerializer {
@@ -12,12 +13,26 @@ object EventSerializer {
     private val format = Json {
         serializersModule = SerializersModule {
             polymorphic(Event::class) {
+                // CreateOrderSagaEvents
+                subclass(VerifyCustomerCommandEvent::class, VerifyCustomerCommandEvent.serializer())
+                subclass(VerifyCustomerReplyEvent::class, VerifyCustomerReplyEvent.serializer())
+                subclass(CreateTicketCommandEvent::class, CreateTicketCommandEvent.serializer())
+                subclass(CreateTicketReplyEvent::class, CreateTicketReplyEvent.serializer())
                 subclass(AuthorizationCommandEvent::class, AuthorizationCommandEvent.serializer())
                 subclass(AuthorizationReplyEvent::class, AuthorizationReplyEvent.serializer())
+                subclass(ApproveTicketCommandEvent::class, ApproveTicketCommandEvent.serializer())
+                subclass(ApplyTicketReplyEvent::class, ApplyTicketReplyEvent.serializer())
             }
         }
     }
 
+    /**
+     * Deserialize a event.
+     * @param event The event serialized as a string.
+     * @return Deserialized event.
+     * @throws IllegalArgumentException If the event is not resolvable to a subclass of event.
+     */
+    @Throws(IllegalArgumentException::class)
     fun deserialize(event: String): Event = format.decodeFromString(event)
     fun serialize(event: Event): String = format.encodeToString(event)
 
--- microservice-patterns/saga_pattern/orchestration/order_service/src/test/kotlin/com/oli/event/EventSerializerTest.kt
+++ microservice-patterns/saga_pattern/orchestration/order_service/src/test/kotlin/com/oli/event/EventSerializerTest.kt
@@ -11,15 +11,17 @@ class EventSerializerTest {
         val event: Event = AuthorizationCommandEvent(1, 2, 3, "test")
         val actual = EventSerializer.serialize(event)
 
-        val expected = "{\"type\":\"AuthorizationCommandEvent\",\"correlationId\":1,\"sagaId\":2,\"userId\":3,\"paymentInfo\":\"test\"}"
+        val expected = "{\"type\":\"com.oli.event.AuthorizationCommandEvent\",\"correlationId\":1,\"sagaId\":2,\"userId\":3,\"paymentInfo\":\"test\"}"
         assertEquals(expected, actual)
     }
 
     @Test
     fun deserializeAuthorizationCommandEventTest() {
-        val actual = EventSerializer.deserialize("{\"type\":\"AuthorizationCommandEvent\",\"correlationId\":1,\"sagaId\":2,\"userId\":3,\"paymentInfo\":\"test\"}")
+        val actual = EventSerializer.deserialize("{\"type\":\"com.oli.event.AuthorizationCommandEvent\",\"correlationId\":1,\"sagaId\":2,\"userId\":3,\"paymentInfo\":\"test\"}")
 
         val expected: Event = AuthorizationCommandEvent(1, 2, 3, "test")
+        assertTrue(actual is SagaEvent)
+        assertTrue(actual is CreateOrderSagaEvent)
         assertTrue(actual is AuthorizationCommandEvent)
         assertEquals(expected, actual)
     }
@@ -38,13 +40,13 @@ class EventSerializerTest {
         val event: Event = AuthorizationReplyEvent(1, 2, false)
         val actual = EventSerializer.serialize(event)
 
-        val expected = "{\"type\":\"AuthorizationReplyEvent\",\"correlationId\":1,\"sagaId\":2,\"result\":false}"
+        val expected = "{\"type\":\"com.oli.event.AuthorizationReplyEvent\",\"correlationId\":1,\"sagaId\":2,\"result\":false}"
         assertEquals(expected, actual)
     }
 
     @Test
     fun deserializeAuthorizationReplyEventTest() {
-        val actual = EventSerializer.deserialize("{\"type\":\"AuthorizationReplyEvent\",\"correlationId\":1,\"sagaId\":2,\"result\":false}")
+        val actual = EventSerializer.deserialize("{\"type\":\"com.oli.event.AuthorizationReplyEvent\",\"correlationId\":1,\"sagaId\":2,\"result\":false}")
 
         val expected: Event = AuthorizationReplyEvent(1, 2, false)
         assertTrue(actual is AuthorizationReplyEvent)
@@ -57,6 +59,9 @@ class EventSerializerTest {
         val serialized = EventSerializer.serialize(event)
         val deserialized = EventSerializer.deserialize(serialized)
 
+        assertTrue(deserialized is SagaEvent)
+        assertTrue(deserialized is CreateOrderSagaEvent)
+        assertTrue(deserialized is AuthorizationReplyEvent)
         assertEquals(event, deserialized)
     }
 }
